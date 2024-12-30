59943c3feae0f5a2aa6dcb0a4320bd58cda6a65c
danielrichardson1 <drich1475@gmail.com>
2024-12-29 23:16:38 -0500

    Initial Commit

---
+++ Duolingo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Duolingo
+//
+//  Created by Daniel Richardson on 12/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Duolingo/DuolingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuolingoApp.swift
+//  Duolingo
+//
+//  Created by Daniel Richardson on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct DuolingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
