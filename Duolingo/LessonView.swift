b31f4586074537813095c796394ed96ca8484d97
danielrichardson1 <drich1475@gmail.com>
2025-01-06 00:02:01 -0500

    Working on lesson view

--- Duolingo/LessonView.swift
+++ Duolingo/LessonView.swift
@@ -6,10 +6,70 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct LessonView: View {
+    
+    @State private var progress = 0.1
+    @Environment(\.presentationMode) var presentationMode : Binding<PresentationMode>
+    let lessonsData = LessonsData.lessons
+    @State var indexLesson : Int = 0
+    @ObservedObject var selectionHandler = OptionSelectionHandler()
+    @State var gridItems = [GridItem(.flexible(), alignment:.center)]
+    @State private var speechSynthesizer : AVSpeechSynthesizer?
+    @State private var displayResultView : Bool = false
+    @State var result : Result = Result(isCorrect: true, answer: "")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading){
+                
+                HStack{
+                    
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }){
+                        Image(systemName: "xmark")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:30)
+                            .foregroundColor(.gray)
+                    }.padding()
+                    
+                    ZStack(alignment: .leading){
+                        Rectangle()
+                            .foregroundColor(Color.gray.opacity(0.3))
+                            .frame(width:300, height:20)
+                        Rectangle()
+                            .foregroundColor(Color.green)
+                            .frame(width:CGFloat(progress*300), height:20)
+                            .cornerRadius(10)
+                    }.cornerRadius(10)
+                    
+                }
+                
+                let text =
+                switch(lessonsData[indexLesson].type){
+                case LessonType.wordWithSound:
+                    "What sound does this make?"
+                case LessonType.onlyText:
+                    "Select the correct letters for \(lessonsData[indexLesson].question)"
+                case LessonType.onlySound:
+                    "What do you hear?"
+                default:
+                    "Tap the matching pairs"
+                }
+                
+                Text(text)
+                    .font(.system(size:25))
+                    .bold()
+                    .foregroundColor(.black)
+                    .padding(.leading, 10)
+                
+                Spacer().frame(height:50)
+            }
+            
+        }.navigationBarBackButtonHidden(true)
     }
 }
 
--- Duolingo/OptionsCardView.swift
+++ Duolingo/OptionsCardView.swift
@@ -9,32 +9,40 @@ import SwiftUI
 
 struct OptionsCardView: View {
     
-    var index : Int = 0
-    var selectionObject : OptionSelectionHandler = OptionSelectionHandler()
-    var word : String = "poo"
-    var type : LessonType = .wordWithSound
+    var index : Int
+    @ObservedObject var selectionObject : OptionSelectionHandler
+    var word : String
+    var type : LessonType
     
     
     var body: some View {
-        Button(action: {}) {
-            
-        }
-        ZStack{
-            Text(word)
-                .font(.system(size:30))
-                .foregroundColor(.black)
-                .bold()
-                .multilineTextAlignment(.leading)
-            if type == .wordWithSound {
-                RoundedRectangle(cornerRadius:20)
-                    .stroke(selectionObject.selectedEntry[index] ? .green : .gray, lineWidth:2)
+        Button(action: {
+            selectionObject[index].toggle()
+        }) {
+            ZStack{
+                Text(word)
+                    .font(.system(size:30))
+                    .foregroundColor(.black)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                if type == .wordWithSound {
+                    RoundedRectangle(cornerRadius:20)
+                        .stroke(selectionObject.selectedEntry[index] ? .green : .gray, lineWidth:2)
+                        .frame(width:UIScreen.main.bounds.width-20, height:80)
+                        .padding([.leading, .trailing], 15)
+                        .disabled(!selectionObject.selectedEntry[index])
+                } else {
+                    RoundedRectangle(cornerRadius:20)
+                        .stroke(selectionObject.selectedEntry[index] ? .green : .gray, lineWidth:2)
+                        .frame(width:150, height:150)
+                        .padding([.leading, .trailing], 15)
+                        .disabled(!selectionObject.selectedEntry[index])
+                }
             }
-                
         }
     }
 }
 
-
 #Preview {
-    OptionsCardView()
+    OptionsCardView(index: 0, selectionObject: OptionSelectionHandler(), word: "ha", type: .matchingPairs)
 }
